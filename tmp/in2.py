286e41d257e1c1c13dfad364cbac96d440bdd4cd
Demo User <debian@beaglebone.localdomain>
2019-04-08 04:12:23 +0000

    added libpruio, working on python input

--- tmp/in2.py
+++ tmp/in2.py
@@ -4,7 +4,7 @@ from libpruio import *
 
 import datetime
 
-NUM_SAMPLES = 50000
+NUM_SAMPLES = 2500
 
 CHANNEL_0 = np.zeros(NUM_SAMPLES)
 CHANNEL_1 = np.zeros(NUM_SAMPLES)
@@ -20,7 +20,7 @@ io = pruio_new(PRUIO_DEF_ACTIVE, 0, 0, 0)
 IO = io.contents
 if  IO.Errr: raise AssertionError("pruio_new failed (%s)" % IO.Errr)
 
-if pruio_config(io, 50000, 0b11110, 20001, 0):
+if pruio_config(io, NUM_SAMPLES, 0b11110, 20001, 0):
     raise AssertionError("config failed (%s)" % IO.Errr)
 
 while True:
@@ -41,15 +41,24 @@ while True:
     CHANNEL_2_FFT = fft(CHANNEL_2)/NUM_SAMPLES
     CHANNEL_3_FFT = fft(CHANNEL_3)/NUM_SAMPLES
 
+    CHANNEL_0_FFT[0] = 0
+    CHANNEL_1_FFT[0] = 0
+    CHANNEL_2_FFT[0] = 0
+    CHANNEL_3_FFT[0] = 0
+
     CHANNEL_0_FREQS = fftfreq(len(CHANNEL_0_FFT))
     CHANNEL_1_FREQS = fftfreq(len(CHANNEL_1_FFT))
     CHANNEL_2_FREQS = fftfreq(len(CHANNEL_2_FFT))
     CHANNEL_3_FREQS = fftfreq(len(CHANNEL_3_FFT))
 
+    for i in range(NUM_SAMPLES):
+        print(CHANNEL_1[i])
+
     idx_0 = np.argmax(np.abs(CHANNEL_0_FFT))
     idx_1 = np.argmax(np.abs(CHANNEL_1_FFT))
     idx_2 = np.argmax(np.abs(CHANNEL_2_FFT))
     idx_3 = np.argmax(np.abs(CHANNEL_3_FFT))
+
 
     freq_0 = CHANNEL_0_FREQS[idx_0]
     freq_1 = CHANNEL_1_FREQS[idx_1]
@@ -61,6 +70,8 @@ while True:
     freq_2 = abs(freq_2 * NUM_SAMPLES)
     freq_3 = abs(freq_3 * NUM_SAMPLES)
 
-    print(np.mean(CHANNEL_0), np.mean(CHANNEL_1), np.mean(CHANNEL_2), np.mean(CHANNEL_3))
+    exit()
+
+    #print(np.mean(CHANNEL_0), np.mean(CHANNEL_1), np.mean(CHANNEL_2), np.mean(CHANNEL_3))
 
 pruio_destroy(io)
