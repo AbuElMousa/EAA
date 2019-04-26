274a0d6c8f5b8f568dd8ae77be26b5933e0bfa70
Demo User <debian@beaglebone.localdomain>
2019-04-26 20:31:53 +0000

    testing input

---
+++ localizer/old.py
@@ -0,0 +1,20 @@
+from numpy import zeros, argmax
+from numpy.fft import fft, fftfreq
+from libpruio import *
+
+import datetime
+'''
+class Localizer:
+    def __init__(self, sample_per_iteration, microphones, ):
+        print('cat')
+
+
+class Listener:
+    def __init__(self, ):
+        bitm
+
+l = Localizer()
+'''
+
+while True:
+    pass
--- systemd/localizer.sh
+++ systemd/localizer.sh
@@ -1,1 +1,3 @@
+modprobe -r uio_pruss
+modprobe uio_pruss_extram_pool_sz=0x800000
 /usr/bin/python /home/debian/EAA/localizer/localizer.py
